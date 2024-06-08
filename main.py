d744f060af33c1335bd2a7da45b08fcaf02ed66a
muffindud <ccatlabuga2002@gmail.com>
2024-06-08 17:17:48 +0300

    create basic DroneRPC class

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pika
 import dotenv
 
 from src.TelegramRPC import TelegramRPC
+from src.DroneRPC import DroneRPC
 
 config = dotenv.dotenv_values(".env")
 connection = pika.BlockingConnection(pika.ConnectionParameters(config["RABBITMQ_HOST"]))
 channel = connection.channel()
 
 telegramRPC = TelegramRPC(channel)
+droneRPC = DroneRPC(channel)
 
 def main():
     ...
