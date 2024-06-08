1b66618bd9e160800f5ea9b962bec5d4d338ab10
muffindud <ccatlabuga2002@gmail.com>
2024-06-08 17:19:45 +0300

    create basic GUIRPC class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dotenv
 
 from src.TelegramRPC import TelegramRPC
 from src.DroneRPC import DroneRPC
+from src.GUIRPC import GUIRPC
 
 config = dotenv.dotenv_values(".env")
 connection = pika.BlockingConnection(pika.ConnectionParameters(config["RABBITMQ_HOST"]))
@@ -11,6 +12,7 @@ channel = connection.channel()
 
 telegramRPC = TelegramRPC(channel)
 droneRPC = DroneRPC(channel)
+guiRPC = GUIRPC(channel)
 
 def main():
     ...
